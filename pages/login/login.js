3d6c7769ff95308109865a9bc82280fa3cc771e7
diaochan <diaochan@seatent.com>
2026-06-01 11:03:06 +0800

    微信登录

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,37 +3,55 @@ const { setToken } = require('../../utils/auth');
 
 Page({
   data: {
-    phone: '',
-    password: '',
     inviteCode: '',
-    showInvite: false,
+    agreed: false,
     loading: false,
   },
 
-  onPhoneInput(e)    { this.setData({ phone: e.detail.value }); },
-  onPasswordInput(e) { this.setData({ password: e.detail.value }); },
-  onInviteInput(e)   { this.setData({ inviteCode: e.detail.value }); },
-
-  toggleInvite() {
-    this.setData({ showInvite: !this.data.showInvite });
+  onInviteInput(e) {
+    this.setData({ inviteCode: e.detail.value });
   },
 
-  async onLogin() {
-    const { phone, password, inviteCode, loading } = this.data;
+  toggleAgreed() {
+    this.setData({ agreed: !this.data.agreed });
+  },
+
+  onAgreementTap() {
+    wx.navigateTo({ url: '/pages/agreement/agreement' });
+  },
+
+  onGuestLogin() {
+    const pages = getCurrentPages();
+    const prevPage = pages[pages.length - 2];
+    if (prevPage) {
+      wx.navigateBack();
+    } else {
+      wx.switchTab({ url: '/pages/index/index' });
+    }
+  },
+
+  async onWxLogin() {
+    const { agreed, inviteCode, loading } = this.data;
     if (loading) return;
 
-    if (!/^1[3-9]\d{9}$/.test(phone)) {
-      return wx.showToast({ title: '请输入正确的手机号', icon: 'none' });
-    }
-    if (password.length < 6) {
-      return wx.showToast({ title: '密码不少于6位', icon: 'none' });
+    if (!agreed) {
+      return wx.showToast({ title: '请先阅读并同意用户协议', icon: 'none' });
     }
 
     this.setData({ loading: true });
     try {
-      const body = { phone, password };
+      // 获取微信登录 code
+      const { code } = await new Promise((resolve, reject) => {
+        wx.login({
+          success: resolve,
+          fail: reject,
+        });
+      });
+
+      const body = { code };
       if (inviteCode) body.inviteCode = inviteCode;
 
+      // 调用后端微信登录接口
       const data = await http.post('/api/h5/auth/login', body);
       setToken(data.token);
 
@@ -47,6 +65,8 @@ Page({
       } else {
         wx.switchTab({ url: '/pages/mine/mine' });
       }
+    } catch (err) {
+      // 请求失败由 request.js 统一 toast，此处仅重置 loading
     } finally {
       this.setData({ loading: false });
     }
