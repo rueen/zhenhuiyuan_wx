8ef88594c9986d6c9ddb5020cc24706ab57cdae0
diaochan <diaochan@seatent.com>
2026-06-01 16:06:21 +0800

    icon

---
+++ components/icon/icon.js
@@ -0,0 +1,16 @@
+Component({
+  properties: {
+    name: {
+      type: String,
+      value: ''
+    },
+    size: {
+      type: Number,
+      value: 16
+    },
+    color: {
+      type: String,
+      value: 'inherit'
+    }
+  }
+})
