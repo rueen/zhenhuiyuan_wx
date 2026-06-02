06b033e8534ee6c4e0060d1c397d02f7fe88a25f
diaochan <diaochan@seatent.com>
2026-06-02 10:38:50 +0800

    导航栏

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -2,9 +2,9 @@ Component({
   data: {
     selected: 0,
     list: [
-      { pagePath: '/pages/home/home',  text: '首页',  icon: 'home' },
-      { pagePath: '/pages/shop/shop',  text: '商店',  icon: 'shop' },
-      { pagePath: '/pages/mine/mine',  text: '我的',  icon: 'mine' },
+      { pagePath: '/pages/home/home', text: '首页', iconPath: '/images/svg/home.svg',  selectedIconPath: '/images/svg/home_active.svg' },
+      { pagePath: '/pages/shop/shop', text: '商店', iconPath: '/images/svg/shop.svg',  selectedIconPath: '/images/svg/shop_active.svg' },
+      { pagePath: '/pages/mine/mine', text: '我的', iconPath: '/images/svg/mine.svg',  selectedIconPath: '/images/svg/mine_active.svg' },
     ],
   },
 
